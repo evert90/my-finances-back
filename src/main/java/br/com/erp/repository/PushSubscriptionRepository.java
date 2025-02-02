b992540153c2704d170f2b8cdd10cf42a520975b
Éverton <dev@evert90.com>
2025-02-02 00:44:04 -0300

    feat: notification test + asset notification only active

--- src/main/java/br/com/erp/repository/PushSubscriptionRepository.java
+++ src/main/java/br/com/erp/repository/PushSubscriptionRepository.java
@@ -1,10 +1,14 @@
 package br.com.erp.repository;
 
 import br.com.erp.entity.PushSubscriptionEntity;
+import br.com.erp.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PushSubscriptionRepository extends JpaRepository<PushSubscriptionEntity, Long> {
     PushSubscriptionEntity findByEndpointAndAuthKeyAndP256dhKey(String endpoint, String authKey, String p256dhKey);
+    List<PushSubscriptionEntity> findByUser(UserEntity user);
 }
