6248b55b6afeebce417427db4cbe6122f1040a9c
Éverton <dev@evert90.com>
2025-03-25 20:18:12 -0300

    feat: env keepalive

--- src/main/java/br/com/erp/schedule/JobKeepAlive.java
+++ src/main/java/br/com/erp/schedule/JobKeepAlive.java
@@ -3,6 +3,7 @@ package br.com.erp.schedule;
 import br.com.erp.client.KeepAliveClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +14,15 @@ public class JobKeepAlive {
 
     private final KeepAliveClient client;
 
+    @Value("${app.keepalive:false}")
+    private boolean keepAlive;
+
     @Scheduled(fixedDelay = 60000 * 20) //20 MINUTES
     public void run() {
-        client.ping();
+        if(keepAlive) {
+            log.info("Ping");
+            client.ping();
+        }
     }
-
 
 }
